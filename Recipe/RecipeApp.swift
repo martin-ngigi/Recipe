64ee1b7e602506ef1eb334a96211e9fc73593fda
Martin Wainaina <martinwainaina001@gmail.com>
2025-03-12 16:20:19 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Martin on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
